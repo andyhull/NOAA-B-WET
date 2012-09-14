1797a0f53302f38f3acb19962bb27d90a61e39a5
Andy Hull <andyhull3@gmail.com>
2012-09-14 16:33:23 -0700

    Rewrote results view to use php for processing

--- drupal/sites/all/themes/mwee/templates/views-view-table--grantee-survey--page-1.tpl.php
+++ drupal/sites/all/themes/mwee/templates/views-view-table--grantee-survey--page-1.tpl.php
@@ -18,64 +18,101 @@
  */
 ?>
 <?php
-$js_array = json_encode($rows);
+$allResults = new stdClass();
+//loop through rows
+foreach ($rows as $key =>$field){
+  //get the question field names
+  foreach($field as $question => $questionKey) {
+    if($question !=='title') {
+      // create the question object
+      if(!property_exists($allResults, $question)){
+          $allResults->$question = new stdClass();
+          $resultArray = $allResults->$question;
+          //add a type so we can distinguish different questions
+          $allResults->$question->type = $field_classes[$question][$key];
+        } else {
+          $resultArray = $allResults->$question;
+        }
+      $newKey = (string) $question.$questionKey;
+      //add in the values to the question object
+      if(!property_exists($resultArray, $newKey)){
+        $resultArray->$newKey = 1;
+      } else {
+        $resultArray->$newKey += 1;
+      }
+    }
+  }
+}
+
+// $js_array = json_encode($rows);
 $header_array = json_encode($header);
-echo "<script>var allData = ". $js_array . "; var dataLabels = ". $header_array.";</script>";
+$json_array = json_encode($allResults);
+echo "<script>var dataLabels = ". $header_array.";var resultData = ". $json_array ."; console.log(resultData);</script>";
 ?>
 <div id="mainResults"></div>
 <script>
 // We define a function that takes one parameter named $.
 (function ($) { 
   
-  var resultsArray = new Object()
-  var fields = Object.keys(allData[0])
-  for(field in fields){
-    if(fields[field]!=='title'){
-      if(!resultsArray[fields[field]]){
-        resultsArray[fields[field]] = new Object()
-      }
-      for(data in allData){
-        var newData = allData[data][fields[field]]
-        if(!resultsArray[fields[field]][newData]){
-          resultsArray[fields[field]][newData] = 1
-        } else {
-          resultsArray[fields[field]][newData] = parseFloat(resultsArray[fields[field]][newData]+1)
-        }
-      }
-    }
-  }
-  console.log(resultsArray)
-  for(result in resultsArray) {
+  // var resultsArray = new Object()
+  // var fields = Object.keys(allData[0])
+  // for(field in fields){
+  //   if(fields[field]!=='title'){
+  //     if(!resultsArray[fields[field]]){
+  //       resultsArray[fields[field]] = new Object()
+  //     }
+  //     for(data in allData){
+  //       var newData = allData[data][fields[field]]
+  //       if(!resultsArray[fields[field]][newData]){
+  //         resultsArray[fields[field]][newData] = 1
+  //       } else {
+  //         resultsArray[fields[field]][newData] = parseFloat(resultsArray[fields[field]][newData]+1)
+  //       }
+  //     }
+  //   }
+  // }
+  for(result in resultData) {
     var sum = 0;
     var unanswered = 0;
     var question = dataLabels[result]
     //get the total of answered and unanswered questions
-    $.each(resultsArray[result],function(i){
-      var answer = parseFloat(this)
-      if(i == ''){
-        unanswered = parseFloat(this)
-      } else {
-        sum+=parseFloat(answer)
+    $.each(resultData[result],function(i){
+      var re1 = new RegExp(result,"g");
+      var cleanLabel = i.replace(re1, '')
+      if(cleanLabel !=='type'){
+        var answer = parseFloat(this)
+        if(cleanLabel == ''){
+          unanswered = parseFloat(this)
+        } else {
+          sum+=parseFloat(answer)
+        }
       }
     });
+
     $('#mainResults').append('<div id="'+result+'"><div class="questionTitle">'+question+'</div><div class="bar"></div><div class="barLabel"><span class="likertStart">1 - Not at all</span><span class="likertEnd">To a great extent - 5</div><div id="'+result+'More" class="btn">See Details</div></div>')
-    for(data in resultsArray[result]){
-      // console.log(resultsArray[result][data])
-      if([data] == '') {
-      $('#'+result).append('<div class="'+result+'More">Unanswered:<br/>Count: '+resultsArray[result][data]+'</div>')  
+    // console.log(Object.keys(resultData[result]))
+    for(data in resultData[result]){
+      if(resultData[result][data] == "number") {
+        // console.log("number")
       } else {
-        var percent = parseFloat((resultsArray[result][data]/sum )* 100)
-        $('#'+result).append('<div class="'+result+'More">Answer '+data+':<br/>Count: '+resultsArray[result][data]+' Percent of total: '+Math.round(percent)+'%</div>')
-        $('.bar', '#'+result).append('<span class="color'+data+' '+result+'bar'+data+'" style="width:'+percent+'%;" rel="tooltip" data-placement="top" data-original-title="'+data+'">&nbsp;<div class="more">Value: '+data+' Count: '+resultsArray[result][data]+' ('+Math.round(percent)+'% of total)</div></span>')
+        var re = new RegExp(result,"g");
+        var cleanData = data.replace(re, '')
+        if(cleanData == '') {
+        $('#'+result).append('<div class="'+result+'More">Unanswered:<br/>Count: '+resultData[result][data]+'</div>')  
+        } else {
+          var percent = parseFloat((resultData[result][data]/sum )* 100)
+          $('#'+result).append('<div class="'+result+'More">Answer '+cleanData+':<br/>Count: '+resultData[result][data]+' Percent of total: '+Math.round(percent)+'%</div>')
+          $('.bar', '#'+result).append('<span class="color'+cleanData+' '+result+'bar'+cleanData+'" style="width:'+percent+'%;" rel="tooltip" data-placement="top" data-original-title="'+data+'">&nbsp;<div class="more">Value: '+cleanData+' Count: '+resultData[result][data]+' ('+Math.round(percent)+'% of total)</div></span>')
+        }
+        $('.'+result+'More').hide()
+        $('.more', '.'+result+'bar'+cleanData).hide()
+        $('.'+result+'bar'+cleanData).mouseover(function() {
+          $('.more', this).show();
+        })
+        $('.'+result+'bar'+cleanData).mouseout(function() {
+          $('.more', this).hide();
+        })
       }
-      $('.'+result+'More').hide()
-      $('.more', '.'+result+'bar'+data).hide()
-      $('.'+result+'bar'+data).mouseover(function() {
-        $('.more', this).show();
-      })
-      $('.'+result+'bar'+data).mouseout(function() {
-        $('.more', this).hide();
-      })
     }
     $('#'+result+'More').click(function(){
       var resultToggle = $(this).attr('id')
