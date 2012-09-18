2fc7683ea40f0f4ea2f5db93618ba147970f3fe2
Andy Hull <andyhull3@gmail.com>
2012-09-18 10:55:13 -0700

    Updating the results view

--- drupal/sites/all/themes/mwee/templates/views-view-table--grantee-survey--page-1.tpl.php
+++ drupal/sites/all/themes/mwee/templates/views-view-table--grantee-survey--page-1.tpl.php
@@ -74,11 +74,13 @@ echo "<script>var dataLabels = ". $header_array.";var resultData = ". $json_arra
   for(result in resultData) {
     var sum = 0;
     var unanswered = 0;
+    var cleanLabel;
     var question = dataLabels[result]
     //get the total of answered and unanswered questions
     $.each(resultData[result],function(i){
+      //remove the field name from the value label
       var re1 = new RegExp(result,"g");
-      var cleanLabel = i.replace(re1, '')
+      cleanLabel = i.replace(re1, '')
       if(cleanLabel !=='type'){
         var answer = parseFloat(this)
         if(cleanLabel == ''){
@@ -89,29 +91,32 @@ echo "<script>var dataLabels = ". $header_array.";var resultData = ". $json_arra
       }
     });
 
-    $('#mainResults').append('<div id="'+result+'"><div class="questionTitle">'+question+'</div><div class="bar"></div><div class="barLabel"><span class="likertStart">1 - Not at all</span><span class="likertEnd">To a great extent - 5</div><div id="'+result+'More" class="btn">See Details</div></div>')
+    $('#mainResults').append('<div id="'+result+'"><div class="questionTitle">'+question+'</div>')
     // console.log(Object.keys(resultData[result]))
+    var format = Array();
     for(data in resultData[result]){
-      if(resultData[result][data] == "number") {
-        // console.log("number")
-      } else {
-        var re = new RegExp(result,"g");
-        var cleanData = data.replace(re, '')
-        if(cleanData == '') {
-        $('#'+result).append('<div class="'+result+'More">Unanswered:<br/>Count: '+resultData[result][data]+'</div>')  
-        } else {
-          var percent = parseFloat((resultData[result][data]/sum )* 100)
-          $('#'+result).append('<div class="'+result+'More">Answer '+cleanData+':<br/>Count: '+resultData[result][data]+' Percent of total: '+Math.round(percent)+'%</div>')
-          $('.bar', '#'+result).append('<span class="color'+cleanData+' '+result+'bar'+cleanData+'" style="width:'+percent+'%;" rel="tooltip" data-placement="top" data-original-title="'+data+'">&nbsp;<div class="more">Value: '+cleanData+' Count: '+resultData[result][data]+' ('+Math.round(percent)+'% of total)</div></span>')
-        }
-        $('.'+result+'More').hide()
-        $('.more', '.'+result+'bar'+cleanData).hide()
-        $('.'+result+'bar'+cleanData).mouseover(function() {
-          $('.more', this).show();
-        })
-        $('.'+result+'bar'+cleanData).mouseout(function() {
-          $('.more', this).hide();
-        })
+      switch (format[0]) {
+        case 'number':
+          if(!$('#'+result+'More').length) {
+            //add the formatted label and more button
+            $('#' + result).append('<div class="bar"></div><div class="barLabel"><span class="likertStart">1 - Not at all</span><span class="likertEnd">To a great extent - 5</div><div id="'+result+'More" class="btn">See Details</div></div>')  
+          }
+          createNumber(resultData[result][data], result, data, sum)
+          format = resultData[result]['type'].split(" ")
+          break;
+        case 'text':
+          if(format[1] == "structured") {
+            if(!$('#'+result+'More').length) {
+            //add the formatted label and more button
+            $('#' + result).append('<div class="bar"></div><div class="barLabel"><span class="likertStart"></span><span class="likertEnd"></div><div id="'+result+'More" class="btn">See Details</div></div>')  
+            }
+            createText(resultData[result][data], result, data, sum, 'structured')
+          }
+          createText(resultData[result][data], result, data, sum)
+          format = resultData[result]['type'].split(" ")
+          break; 
+        default:
+          format = resultData[result]['type'].split(" ")      
       }
     }
     $('#'+result+'More').click(function(){
@@ -119,7 +124,61 @@ echo "<script>var dataLabels = ". $header_array.";var resultData = ". $json_arra
       $('.'+resultToggle).toggle()
     })
   }
-  // $("[rel=tooltip]").tooltip();
+
+//formatting functions
+  // for all numbers
+  function createNumber(resultData, field, data, sum) {
+    var re = new RegExp(field,"g");
+    var cleanData = data.replace(re, '')
+    if(cleanData == '') {
+    $('#'+field).append('<div class="'+field+'More">Unanswered:<br/>Count: '+resultData+'</div>')  
+    } else {
+      var percent = parseFloat((resultData/sum )* 100)
+      $('#'+result).append('<div class="'+result+'More">Answer '+cleanData+':<br/>Count: '+resultData+' Percent of total: '+Math.round(percent)+'%</div>')
+      $('.bar', '#'+result).append('<span class="color'+cleanData+' '+result+'bar'+cleanData+'" style="width:'+percent+'%;" rel="tooltip" data-placement="top" data-original-title="'+data+'">&nbsp;<div class="more">Value: '+cleanData+' Count: '+resultData+' ('+Math.round(percent)+'% of total)</div></span>')
+    }
+    $('.'+result+'More').hide()
+    $('.more', '.'+result+'bar'+cleanData).hide()
+    $('.'+result+'bar'+cleanData).mouseover(function() {
+      $('.more', this).show();
+    })
+    $('.'+result+'bar'+cleanData).mouseout(function() {
+      $('.more', this).hide();
+    })
+  }
+
+  //for all strings
+  function createText(resultData, field, data, sum, structured) {
+    var re = new RegExp(field,"g");
+    var cleanData = data.replace(re, '')
+    if (!structured ) {
+      structured = ''
+      if(cleanData == '') {
+        $('#'+field).append('<div class="'+field+'More">Unanswered:<br/>Count: '+resultData+'</div>')  
+      } else {
+        $('#'+result).append('<div class="'+result+'More">Answer '+cleanData+' ('+resultData+')</div>')
+      }
+    } else {
+      if(cleanData == '') {
+        $('#'+field).append('<div class="'+field+'More">Unanswered:<br/>Count: '+resultData+'</div>')  
+      } else {
+        var percent = parseFloat((resultData/sum )* 100)
+        console.log(cleanData.replace(', ', ''))
+        $('#'+result).append('<div class="'+result+'More">Answer '+cleanData+':<br/>Count: '+resultData+' Percent of total: '+Math.round(percent)+'%</div>')
+        $('.bar', '#'+result).append('<span class="color'+cleanData.replace(', ', '')+' '+result+'bar'+cleanData.replace(', ', '')+'" style="width:'+percent+'%;" rel="tooltip" data-placement="top" data-original-title="'+data+'">&nbsp;<div class="more">Value: '+cleanData+' Count: '+resultData+' ('+Math.round(percent)+'% of total)</div></span>')
+      }
+      $('.'+result+'More').hide()
+      $('.more', '.'+result+'bar'+cleanData.replace(', ', '')).hide()
+      $('.'+result+'bar'+cleanData.replace(', ', '')).mouseover(function() {
+        $('.more', this).show();
+      })
+      $('.'+result+'bar'+cleanData.replace(', ', '')).mouseout(function() {
+        $('.more', this).hide();
+      })
+    }
+    // $('.'+result+'More').hide()
+  }
+
 // Here we immediately call the function with jQuery as the parameter.
 }(jQuery));
 
